c05a203ceecccc0f507a1200f423f046b3b2f701
William Sedlacek <william.sedlacek@icloud.com>
2026-06-06 16:39:06 -0700

    feat: add vitest support

--- lua/plugins/neotest.lua
+++ lua/plugins/neotest.lua
@@ -55,31 +55,51 @@ return {
     "antoinemadec/FixCursorHold.nvim",
     "rouge8/neotest-rust",
     "haydenmeade/neotest-jest",
+    "marilari88/neotest-vitest",
     "nvim-neotest/nvim-nio",
   },
   config = function()
+    local function find_config(filename)
+      local dir = vim.fn.expand "%:p:h"
+      local found = vim.fn.findfile(filename, dir .. ";")
+      if found ~= "" then
+        return vim.fn.fnamemodify(found, ":p")
+      end
+    end
+
+    local function config_cwd(filename)
+      local cfg = find_config(filename)
+      if cfg then return vim.fn.fnamemodify(cfg, ":h") end
+      return vim.fn.getcwd()
+    end
+
     require("neotest").setup {
       adapters = {
         require "neotest-jest" {
           jestConfigFile = function()
-            local file = vim.fn.expand "%:p"
-            if string.find(file, "/packages/") then
-              return string.match(file, "(.-/[^/]+/)src") .. "jest.config.ts"
-            end
-            if string.find(file, "/libs/") then
-              return string.match(file, "(.-/[^/]+/)src") .. "jest.config.ts"
-            end
-            return vim.fn.getcwd() .. "/jest.config.ts"
+            return find_config "jest.config.ts"
+              or find_config "jest.config.js"
+              or (vim.fn.getcwd() .. "/jest.config.ts")
           end,
           cwd = function()
-            local file = vim.fn.expand "%:p"
-            if string.find(file, "/packages/") then
-              return string.match(file, "(.-/[^/]+/)src")
-            end
-            if string.find(file, "/libs/") then
-              return string.match(file, "(.-/[^/]+/)src")
-            end
-            return vim.fn.getcwd()
+            return config_cwd "jest.config.ts"
+              or config_cwd "jest.config.js"
+              or vim.fn.getcwd()
+          end,
+        },
+        require "neotest-vitest" {
+          vitestConfigFile = function()
+            return find_config "vitest.config.ts"
+              or find_config "vitest.config.js"
+              or find_config "vite.config.ts"
+              or find_config "vite.config.js"
+          end,
+          cwd = function()
+            return config_cwd "vitest.config.ts"
+              or config_cwd "vitest.config.js"
+              or config_cwd "vite.config.ts"
+              or config_cwd "vite.config.js"
+              or vim.fn.getcwd()
           end,
         },
         require "neotest-rust" {
